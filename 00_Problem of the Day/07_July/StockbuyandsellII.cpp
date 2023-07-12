3e61559dbbf414702d9bbd29595b9f024d013f6a
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-07-12 21:35:15 +0530

    05 - Stock buy and sell II
    Medium

---
+++ 00_Problem of the Day/07_July/StockbuyandsellII.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+  public:
+  
+    int stockBuyAndSell(int n, vector<int> &prices) {
+        
+        int sum = 0;
+        
+        for(int i=0; i<n-1; i++) {
+            if(prices[i] < prices[i+1]) 
+               sum += (prices[i+1] - prices[i]);
+        }
+        return sum;
+    }
+};
