b0ab8463f3f57262a3fb252f9ffbcc9af62d7dba
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-08-06 22:12:18 +0530

    06 - String Permutations

---
+++ 00_Problem of the Day/08_August/StringPermutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+    void solve(int start, string &str, vector<string> &ans) {
+        if(start==str.size()) {
+            ans.push_back(str);
+            return;
+        }
+        
+        for(int i=start; i<str.size(); i++){
+            swap(str[i],str[start]);
+            solve( start+1, str, ans );
+            swap(str[i],str[start]);
+        }
+    }
+        
+    vector<string> permutation(string str)
+    {
+        vector<string> ans;
+        solve(0,str,ans);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
