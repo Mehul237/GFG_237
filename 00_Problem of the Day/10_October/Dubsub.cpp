5d6efc37ddda23550a0f4338f9b8bf11638bd0b8
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-10-13 18:49:33 +0530

    13 - Duplicate subtree in Binary Tree

---
+++ 00_Problem of the Day/10_October/Dubsub.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+  
+    string help(Node*root, unordered_map<string, int>&m) {
+        if(!root) return "";
+        string s = "";
+        s += help(root -> left, m);
+        s.push_back(root -> data + '0');
+        s += help(root -> right, m);
+        m[s]++;
+        return s;
+    }
+    
+    int dupSub(Node *root) {
+        unordered_map<string, int> m;
+        help(root, m);
+        
+        for(auto it = m.begin(); it != m.end(); ++it) {
+            if(it -> first.size() > 1 && it -> second > 1) return 1;
+        }
+        return 0;
+    }
+};
