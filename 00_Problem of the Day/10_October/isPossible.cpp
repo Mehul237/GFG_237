03ce21a69236f06928ab08d29b32a9c246482075
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-10-20 23:10:42 +0530

    20 - Form a number divisible by 3 using array digits

---
+++ 00_Problem of the Day/10_October/isPossible.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int isPossible(int N, int arr[]) {
+        
+        int sum = 0;
+        
+        for(int i = 0; i < N; i++) {
+            int it = arr[i];
+            while(it > 0) {
+                sum += it % 10;
+                it /= 10;
+            }
+        }
+        return sum % 3 == 0;
+    }
+};
