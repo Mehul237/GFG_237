e175aad1a698f1715722de66b0de601b40fcdc92
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-11-02 18:54:03 +0530

    02 - Minimum distance between two numbers

---
+++ 00_Problem of the Day/11_November/minDist.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+   int minDist(int a[], int n, int x, int y) {
+       
+      int xi = -1;
+      int yi = -1;
+      int ans = INT_MAX;
+      
+      for (int i = 0; i < n; i++) {
+         if (a[i] == x) xi = i;
+         if (a[i] == y) yi = i;
+         if (xi != -1 && yi != -1) {
+            ans = min(ans, abs(xi - yi));
+        }
+    }
+    return ans == INT_MAX ? -1 : ans;
+  }
+};
