7e9306cd88af80cc95f113f4976f2443a92b7ea9
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-11-24 08:50:05 +0530

    24 - Pascal Triangle

--- 00_Problem of the Day/11_November/nthRowofPascalTriangle.cpp
+++ 00_Problem of the Day/11_November/nthRowofPascalTriangle.cpp
@@ -6,7 +6,7 @@
         for(int i=0; i<n; i++) {
             vector<long long> curr(i+1, 1);
             for(int j=1; j<i; j++) {
-                curr[j] = (prev[j-1]+prev[j])%mod;
+                curr[j] = (prev[j-1] + prev[j]) % mod;
             }    
             prev = curr;
         }
